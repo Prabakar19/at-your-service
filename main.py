5d77d2e33e79871357c25448901c674ad0d20e8e
Prabakar19 <praphakar119@gmail.com>
2023-07-27 03:36:28 +0530

    update start command to code start

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import uvicorn
 from fastapi import FastAPI
 from service.cutomer_serivce import CustomerService
 app = FastAPI()
 
 
 @app.get("/api/customer/{customer_id}")
-def get_customer(customer_id: int):
+async def get_customer(customer_id: str):
     customer_service = CustomerService()
-    return customer_service.get_customer(customer_id)
+    return await customer_service.get_customer(customer_id)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=9098)
