8baf7547e6bc79291efe22b9345b9327967f621d
Prabakar19 <praphakar119@gmail.com>
2023-07-27 17:31:19 +0530

    modify customer service for database call

--- service/cutomer_serivce.py
+++ service/cutomer_serivce.py
@@ -1,7 +1,10 @@
+from dao.customer_dao import CustomerDao
 
 
 class CustomerService:
-    CUSTOMERS = {1: 'praba', 2: 'priyanka', 3: 'surya'}
+    CUSTOMERS = {'1': 'praba', '2': 'priyanka', '3': 'surya'}
 
-    def get_customer(self, customer_id: int):
-        return self.CUSTOMERS.get(customer_id, 'No Customer Found')
+    async def get_customer(self, customer_id: str):
+        # customer = await CustomerDao.get_customer_by_id(customer_id)
+        customer = self.CUSTOMERS.get(customer_id, 'No Customer Found')
+        return customer
