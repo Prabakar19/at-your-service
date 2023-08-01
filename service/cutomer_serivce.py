e63bd668d76b80757695aac908f14713a2609dfe
Prabakar19 <praphakar119@gmail.com>
2023-08-01 15:40:10 +0530

    add customer

--- service/cutomer_serivce.py
+++ service/cutomer_serivce.py
@@ -1,4 +1,5 @@
 from dao.customer_dao import CustomerDao
+from model.customer import Customer
 
 
 class CustomerService:
@@ -6,3 +7,6 @@ class CustomerService:
     async def get_customer(self, customer_id: str):
         customer = await CustomerDao.get_customer_by_id(customer_id)
         return customer
+
+    async def add_customer(self, customer: Customer):
+        await CustomerDao.add_customer(customer)
